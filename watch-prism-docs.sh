d543c7999d7dad4d13a729d92b5b2afbf1c80827
Jeswin <jeswinpk@agilehead.com>
2020-07-24 19:52:15 +0530

    Script to monitor prism docs for changes

---
+++ watch-prism-docs.sh
@@ -0,0 +1,20 @@
+bashocmd=$(cat <<"EOF"
+-j
+  x.split(" ")
+-j 
+  (
+    x[1] === "MODIFY" || x[1] === "CREATE" 
+      ? `cp ${x[0]}${x[2]} docs/prism/${x[2]}`
+      : x[1] === "DELETE" 
+        ? `rm docs/prism/${x[2]}` 
+        : undefined
+  )
+-f 
+  x !== undefined
+EOF
+)
+
+while true; do
+  (inotifywait -e create -e delete -e modify  -r ../prism-docs/docs/ && echo $FILE) |
+    basho "$bashocmd" -l x -e '${x}'
+done
